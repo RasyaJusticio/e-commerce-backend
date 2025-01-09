77a39c8ac7314ca92b60a41bf471fbb82e695385
rasyajusticio <rasyajusticio.public@gmail.com>
2025-01-09 09:33:20 +0700

    revert "feat(auth): create login endpoint"
    This reverts commit b4efd4db6ada7f68c2fb84509ba1d8faebbe170e.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
@@ -20,41 +18,6 @@ class AuthController extends Controller
             'email' => $validatedData['email'],
             'password' => $validatedData['password']
         ]);
-
-        $token = JWTAuth::fromUser($user);
-
-        return response()->json([
-            'status' => 'success',
-            'data' => null
-        ])->cookie(
-            'jwt_token',
-            $token,
-            120,
-            '/',
-            null,
-            false,
-            true
-        );
-    }
-
-    public function login(LoginRequest $request)
-    {
-        $validatedData = $request->validated();
-
-        $user = User::query()
-            ->where(['email' => $validatedData['email']])
-            ->first();
-
-        if (!$user || !Hash::check($validatedData['password'], $user->password)) {
-            return response()->json([
-                'status' => 'fail',
-                'data' => [
-                    'errors' => [
-                        'message' => 'Email or password is incorrect'
-                    ]
-                ]
-            ]);
-        }
 
         $token = JWTAuth::fromUser($user);
 
--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
 });
