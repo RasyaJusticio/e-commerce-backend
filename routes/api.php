5d58bce236bc53ca03e8f00f9f69ac34cf152446
rasyajusticio <rasyajusticio.public@gmail.com>
2025-01-09 09:33:22 +0700

    revert "feat(auth): create register endpoint"
    This reverts commit 4f8395d9dc4cbfc517a7a0807462deeeb36b7837.

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\AuthController;
-use Illuminate\Support\Facades\Route;
-
-Route::group(['prefix' => 'auth'], function () {
-    Route::post('register', [AuthController::class, 'register']);
-});
