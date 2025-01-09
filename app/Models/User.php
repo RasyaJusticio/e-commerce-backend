8d037f745b3930e8a35feb69e263b439d2914ddf
rasyajusticio <rasyajusticio.public@gmail.com>
2025-01-09 09:33:24 +0700

    revert "feat(auth): add jwt-auth package and initialize it"
    This reverts commit 86e9bd86b4f8a2516ab377b71565c8f9e2275fbf.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,22 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
 
     /**
      * The attributes that are mass assignable.
