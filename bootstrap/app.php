7d597276695b90a4db5bb1994e68f9cc7e7efd6f
rasyajusticio <rasyajusticio.public@gmail.com>
2025-01-09 09:30:02 +0700

    revert: restore state before implementing auth
    This reverts commit 5b1ec52e2205a6d8c790d666ddd46ef74d5891fa.
    The reason why we are reverting it is because we find out that JWT sucks for this type of project.
    That's why we are going with Laravel's authentication method instead (Either with Sanctum or Passport).

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\JwtAuth;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,9 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'jwt-auth' => JwtAuth::class,
-        ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'auth'], function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
-
-    Route::group(['middleware' => ['jwt-auth']], function () {
-        Route::post('logout', [AuthController::class, 'logout']);
-    });
 });
