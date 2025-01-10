ac844f3ffa542a54c409574ca87f3e7506740b3a
rasyajusticio <rasyajusticio.public@gmail.com>
2025-01-10 16:43:07 +0700

    feat(auth): add middleware to attach token from cookie to request headers

---
+++ app/Http/Middleware/AttachTokenFromCookie.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AttachTokenFromCookie
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->hasCookie('access_token') && ! $request->bearerToken()) {
+            $token = $request->cookie('access_token');
+
+            $request->headers->set('Authorization', 'Bearer ' . $token);
+        }
+
+        return $next($request);
+    }
+}
