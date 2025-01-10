10cfeb054125a1eeeefa20afeddf2d8ea1102388
rasyajusticio <rasyajusticio.public@gmail.com>
2025-01-10 16:34:01 +0700

    feat(auth): add login endpoint

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Requests\LoginRequest;
 use App\Models\User;
 use App\Traits\JSendResponse;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -27,5 +29,22 @@ class AuthController extends Controller
         $cookie = cookie('access_token', $token, 60 * 24 * 3, null, null, false, true);
 
         return $this->jsend_success(null, 201)->cookie($cookie);
+    }
+
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $validatedData = $request->validated();
+
+        if (! Auth::attempt($validatedData)) {
+            return $this->jsend_fail([
+                'message' => 'Email or password is incorrect',
+            ], 401);
+        }
+
+        $token = Auth::user()->createToken('auth_token')->accessToken;
+
+        $cookie = cookie('access_token', $token, 60 * 24 * 3, null, null, false, true);
+
+        return $this->jsend_success(null, 200)->cookie($cookie);
     }
 }
