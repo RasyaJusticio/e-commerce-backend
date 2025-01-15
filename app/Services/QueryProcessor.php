af6d1dc52be9081444a0a5e547cb2cb907b53e98
rasyajusticio <rasyajusticio.public@gmail.com>
2025-01-15 15:43:39 +0700

    feat: add filter functionality to QueryProcessor

--- app/Services/QueryProcessor.php
+++ app/Services/QueryProcessor.php
@@ -4,6 +4,23 @@ namespace App\Services;
 
 class QueryProcessor
 {
+    public function filter($query, $filter = [], $case_sensitive = false)
+    {
+        if (count($filter) > 0) {
+            $query->where(function ($query) use ($filter, $case_sensitive) {
+                foreach ($filter as $key => $value) {
+                    if ($case_sensitive) {
+                        $query->orWhere($key, 'LIKE', '%' . $value . '%');
+                    } else {
+                        $query->orWhere($key, 'ILIKE', '%' . $value . '%');
+                    }
+                }
+            });
+        }
+
+        return $query;
+    }
+
     public function paginate($query, $page = 1, $per_page = 10)
     {
         if ($page < 1) {
