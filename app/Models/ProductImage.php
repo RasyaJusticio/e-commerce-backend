1ef0a6a49458684cd6b015c865da04b818b93364
rasyajusticio <rasyajusticio.public@gmail.com>
2025-01-16 15:00:55 +0700

    feat(product): add event and listener for product image deletion

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -2,12 +2,17 @@
 
 namespace App\Models;
 
+use App\Events\ProductImageDeleting;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductImage extends Model
 {
     protected $fillable = [
         'path'
+    ];
+
+    protected $dispatchesEvents = [
+        'deleting' => ProductImageDeleting::class,
     ];
 
     public function product()
