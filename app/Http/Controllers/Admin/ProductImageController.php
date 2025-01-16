18a9e599f31396c73ef20172b9b84bc0f4820c8a
rasyajusticio <rasyajusticio.public@gmail.com>
2025-01-16 14:48:04 +0700

    feat(product): add store method to ProductImageController for image upload

---
+++ app/Http/Controllers/Admin/ProductImageController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\ProductImage;
+use App\Traits\JSendResponse;
+use App\Http\Requests\Admin\Product\ProductImageStoreRequest;
+use Illuminate\Support\Facades\Storage;
+
+class ProductImageController extends Controller
+{
+    use JSendResponse;
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ProductImageStoreRequest $request, Product $product)
+    {
+        $image = $request->file('image');
+
+        $path = Storage::disk('public')->putFileAs('products', $image, $image->hashName());
+        $product->images()->create([
+            'path' => $path,
+        ]);
+
+        return $this->jsend_success(null);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductImage $productImage, Product $product)
+    {
+        //
+    }
+}
